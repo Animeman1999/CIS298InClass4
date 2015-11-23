68f6d2f2881dd97a9c8b01b8ff2028046a3640d1
David Barnes <barnesdavidj@gmail.com>
2015-11-23 16:27:21 -0500

    Work on switching the Crime app over to using an SQLite database. Add and Update methods are added.

--- app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/CrimeLab.java
+++ app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/CrimeLab.java
@@ -1,10 +1,16 @@
 package edu.kvcc.cis298.inclass3.inclass3;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import edu.kvcc.cis298.inclass3.inclass3.database.CrimeBaseHelper;
+import edu.kvcc.cis298.inclass3.inclass3.database.CrimeDbSchema;
+import edu.kvcc.cis298.inclass3.inclass3.database.CrimeDbSchema.CrimeTable;
 
 /**
  * Created by dbarnes on 10/28/2015.
@@ -16,9 +22,11 @@ public class CrimeLab {
     //we will return this variable that holds our instance.
     private static CrimeLab sCrimeLab;
 
-    //A variable to of TYPE List, which is an interface, to hold
-    //A list of TYPE Crime.
-    private List<Crime> mCrimes;
+    //Private variable for the context
+    private Context mContext;
+
+    //private variable for the database that this crimelab will use
+    private SQLiteDatabase mDatabase;
 
     //This is the method that will be used to get an instance of
     //CrimeLab. It will check to see if the current instance in the
@@ -38,42 +46,107 @@ public class CrimeLab {
     //to make an instance, we want them to use the get method
     //declared right above here.
     private CrimeLab(Context context) {
-        //Instanciate a new ArrayList, which is a child class that
-        //Implements the Interface List. Because ArrayList is a child
-        //of List, we can store it in the mCrimes variable which is of
-        //type List, and not ArrayList. (Polymorphism)
-        mCrimes = new ArrayList<>();
 
-        //for loop to populate our arraylist with some dummy data.
-        for (int i=0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
+        //Assign the passed in context a class level one.
+        mContext = context.getApplicationContext();
+
+        //Use the context in conjunction with the CrimeBaseHelper
+        //class that we wrote to get the writeable database.
+        //We didn't write the getWritableDatabase function that
+        //is being called. It came from the parent class that
+        //CrimeBaseHelper inherits from.
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
+
     }
 
+    //Method to add a new crime to the database. This method will get
+    //called when a user clicks on the add button of the toolbar.
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+        //Get the content values that we would like to stick into the
+        //database by sending it the crime that needs to be translated
+        ContentValues values = getContentValues(c);
+
+        //Call the insert method of our class level version of the CrimeBaseHelper
+        //class. We did not write the insert method. It came from the parent class
+        //of CrimeBaseHelper. We are just using it.
+        mDatabase.insert(CrimeTable.NAME, null, values);
+    }
+
+    public void updateCrime(Crime crime) {
+        //Get the UUID out of the crime as a string. This will be used
+        //in the WHERE clause of the SQL to find the row we want to update.
+        String uuidString = crime.getId().toString();
+        //Create the content values that will be used to do
+        //the update of the model
+        ContentValues values = getContentValues(crime);
+
+        //Update a specific crime with the values from the content values
+        //for a crime that has the UUID of the one in uuidString.
+        //
+        //The update method has the following signature:
+        //First Param: Table Name
+        //Second Param: Values to update
+        //Third Param: WHERE clause to know which row to update.
+        //Fourth Param: String array of parameters to bind to the ?'s
+        //in the WHERE clause.
+        //
+        //The finished SQL will look something like:
+        //UPDATE Crimes WHERE uuid = ? SET (param1, param2, ...)
+        //  VALUES (value1, value2, ...);
+        //Where the ? will become the value in uuidstring
+        mDatabase.update(CrimeTable.NAME, values,
+            CrimeTable.Cols.UUID + " = ?",
+            new String[] { uuidString });
     }
 
     //Getter to get the crimes
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     //Method to get a specific crime based on the
     //UUID that is passed in.
     public Crime getCrime(UUID id) {
-        //This is a foreach loop to go through all of the crimes
-        //at each iteration the current crime will be called 'crime'.
-        for (Crime crime : mCrimes) {
-            //If we find a match, return it.
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
+
         //no match, return null.
         return null;
+    }
+
+    //static method to do the work of taking in a crime and creating
+    //a contentValues object that can be used to insert the crime into
+    //the database. The ContentValues class operates as a hash table, or
+    //"key => value" array. The key refers to the column name of the database
+    //and the value refers to the value we would like to put into the database.
+    private static ContentValues getContentValues(Crime crime) {
+        //Make a new ContentValues object
+        ContentValues values = new ContentValues();
+        //Put the UUID converted to a string
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        //Put the title. No conversion is neccessary since it is a string
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        //Put the Date. Note that it has to be changed from a Date object
+        //to a Timestamp. That's why we are calling the getTime method at the
+        //end. The database can only store the Date as a Timestamp
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        //Welcome to the ternary operator. It evaluates an expression
+        //to true or false. If it is true, the value between the ? and :
+        //is used. If it is false, the value after the : is used. This
+        //is like a one line if else statement. We are using it here
+        //change the true / false value into a 0 or 1 since that is how
+        //the database will store a boolean.
+
+        //Here is the long version:
+
+        //int intToUse;
+        //if (crime.isSolved) {
+        //    intToUse = 1;
+        //} else {
+        //    intToUse = 0;
+        //}
+        //values.put(CrimTable.Cols.SOLVED, intToUse);
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
     }
 }
