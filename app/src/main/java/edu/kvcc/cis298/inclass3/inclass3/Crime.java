157367cbe1a166879ea9eb2fd272398b182a6cf0
David Barnes <barnesdavidj@gmail.com>
2015-11-30 16:41:52 -0500

    Finish chapter 14 with SQLite.

--- app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/Crime.java
+++ app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/Crime.java
@@ -14,10 +14,17 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
 
-    //Default constructor
+    //No parameter constructor. Generates a new UUID and then
+    //calls the single parameter constructor to create the new Crime
     public Crime() {
-        //Make a new unique id for this particular crime
-        mId = UUID.randomUUID();
+        //Make a new unique id for this particular crime and use the
+        //other constructor to get it created.
+        this(UUID.randomUUID());
+    }
+
+    //Single parameter constuctor that takes in a UUID an assigns it.
+    public Crime(UUID id) {
+        mId = id;
         mDate = new Date();
     }
 
